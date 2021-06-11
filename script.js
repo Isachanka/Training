e13f98d75d4b2fe9d4a8e5ecc566ca047fef8a64
Vasili Isachanka <isa4enko.vasia@gmail.com>
2021-06-11 22:05:54 +0200

    new function

--- script.js
+++ script.js
@@ -518,3 +518,40 @@ function whoIsNext(names, r){
 
 // alert(whoIsNext(["Sheldon", "Leonard", "Penny", "Rajesh", "Howard"], 7230702951))
 
+
+function validSolution(board){
+    for(let line of board){
+      for(let  i = 1; i <= line.length; i++){  
+        if(line.includes(i) == false || line.includes(0)){
+            return false;
+        }
+      }
+    }
+    for(let k = 0; k < 9; k++){
+        kruta : for(let p = 1; p <= 9; p++){
+            for(let i = 0; i < board.length; i++){
+
+                if(board[i][k] == p ){
+                p++;
+                break kruta;
+                }
+            }
+            return false;
+        }
+    }
+    return true;
+}
+  
+
+/* alert(validSolution([[5, 3, 4, 6, 7, 8, 9, 1, 2], 
+    [6, 7, 2, 1, 9, 5, 3, 4, 8],
+    [1, 9, 8, 3, 4, 2, 5, 6, 7],
+    [8, 5, 9, 7, 6, 1, 4, 2, 3],
+    [4, 2, 6, 8, 5, 3, 7, 9, 1],
+    [7, 1, 3, 9, 2, 4, 8, 5, 6],
+    [9, 6, 1, 5, 3, 7, 2, 8, 4],
+    [2, 8, 7, 4, 1, 9, 6, 3, 5],
+    [3, 4, 5, 2, 8, 6, 1, 7, 9]]))
+
+*/
+
